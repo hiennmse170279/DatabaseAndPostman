88ef70f9939c1081e24b69028f5b67616a567e05
hiennmse170279 <hiennmse170279@fpt.edu.vn>
2024-02-25 16:17:03 +0700

    Add Database and Postman

---
+++ Database/Image.sql
@@ -0,0 +1,6 @@
+INSERT INTO `images` (`id`, `pathUrl`, `pathName`, `projectID`, `typeRoomID`, `createdAt`, `updatedAt`) VALUES
+(1, 'https://res.cloudinary.com/tivas/image/upload/v1707068113/tivas_image/wkramyliri4gimvdxaln.png', 'tivas_image/wkramyliri4gimvdxaln', 1, NULL, '2024-02-05 00:35:55', '2024-02-05 00:35:55');
+INSERT INTO `images` (`id`, `pathUrl`, `pathName`, `projectID`, `typeRoomID`, `createdAt`, `updatedAt`) VALUES
+(2, 'https://res.cloudinary.com/tivas/image/upload/v1707068112/tivas_image/ypsxqthwhyjfunsoiqhu.png', 'tivas_image/ypsxqthwhyjfunsoiqhu', 1, NULL, '2024-02-05 00:35:55', '2024-02-05 00:35:55');
+INSERT INTO `images` (`id`, `pathUrl`, `pathName`, `projectID`, `typeRoomID`, `createdAt`, `updatedAt`) VALUES
+(3, 'https://res.cloudinary.com/tivas/image/upload/v1707068113/tivas_image/jglhgcncmefv1vil8xcj.png', 'tivas_image/jglhgcncmefv1vil8xcj', 2, NULL, '2024-02-05 00:35:55', '2024-02-05 00:35:55');
---
+++ Database/Project.sql
@@ -0,0 +1,16 @@
+INSERT INTO `projects` (`id`, `name`, `description`, `location`, `buildingStatus`, `thumbnailPathUrl`, `thumbnailPathName`, `createdAt`, `updatedAt`) VALUES
+(1, 'Alma', 'Hotel, Villa', 'Vietnam', 1, 'https://res.cloudinary.com/tivas/image/upload/v1707068113/tivas_image/wkramyliri4gimvdxaln.png', 'tivas_image/wkramyliri4gimvdxaln', '2024-01-26 08:59:52', '2024-01-26 16:58:27');
+INSERT INTO `projects` (`id`, `name`, `description`, `location`, `buildingStatus`, `thumbnailPathUrl`, `thumbnailPathName`, `createdAt`, `updatedAt`) VALUES
+(2, 'VinPearl', 'Only Hotel', 'Nha Trang', 1, 'https://res.cloudinary.com/tivas/image/upload/v1707068113/tivas_image/wkramyliri4gimvdxaln.png', 'tivas_image/wkramyliri4gimvdxaln', '2024-01-26 14:18:06', '2024-01-26 14:18:06');
+INSERT INTO `projects` (`id`, `name`, `description`, `location`, `buildingStatus`, `thumbnailPathUrl`, `thumbnailPathName`, `createdAt`, `updatedAt`) VALUES
+(3, '5 Star', 'Only Villa', 'Ninh Thuan', 1, NULL, NULL, '2024-01-26 14:18:06', '2024-01-26 14:18:06');
+INSERT INTO `projects` (`id`, `name`, `description`, `location`, `buildingStatus`, `thumbnailPathUrl`, `thumbnailPathName`, `createdAt`, `updatedAt`) VALUES
+(4, '1 Star', 'Only Villa', 'Vietnam', 2, NULL, NULL, '2024-01-26 16:39:40', '2024-01-26 16:39:40'),
+(5, '2 Star', 'Only Villa', 'Vietnam', 2, NULL, NULL, '2024-01-28 00:16:21', '2024-01-28 00:16:21'),
+(6, '3 Star', 'Only Villa', 'Vietnam', 2, NULL, NULL, '2024-01-28 00:16:21', '2024-01-28 00:16:21'),
+(7, '4 Star', 'Only Villa', 'Vietnam', 2, NULL, NULL, '2024-01-28 00:16:21', '2024-01-28 00:16:21'),
+(8, 'Resort1', 'Only Hotel', 'Vietnam', 3, NULL, NULL, '2024-01-28 00:16:21', '2024-01-28 00:16:21'),
+(9, 'Resort2', 'Only Hotel', 'Vietnam', 3, NULL, NULL, '2024-01-28 00:16:21', '2024-01-28 00:16:21'),
+(10, 'Resort3', 'Hotel, Villa', 'Vietnam', 3, NULL, NULL, '2024-01-28 00:16:21', '2024-01-28 00:16:21'),
+(11, 'Resort4', 'Hotel, Villa', 'Vietnam', 3, NULL, NULL, '2024-01-28 00:16:21', '2024-01-28 00:16:21'),
+(12, 'Resort5', 'Hotel, Villa', 'Vietnam', 3, NULL, NULL, '2024-01-28 00:16:51', '2024-01-28 00:16:51');
