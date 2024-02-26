23170adb9d1fcd1d52bb300c5627524cde80c7c3
Minh Hiển <hiennmse170279@fpt.edu.vn>
2024-02-26 23:32:35 +0700

    Update SQL

---
+++ Database/Users(4).sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
+(1, 'admin', 'admin@gmail.com', '123456', '123456789', 0, NULL, 1, NULL, NULL, '2024-02-25 22:49:51', '2024-02-25 22:49:51');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
+(2, 'staff', 'staff@gmail.com', '123456', '123456789', 0, NULL, 2, NULL, NULL, '2024-02-25 22:50:21', '2024-02-25 22:50:21');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
+(3, 'customer', 'customer@gmail.com', '123456', '123456789', 0, NULL, 3, NULL, NULL, '2024-02-25 22:51:02', '2024-02-25 22:51:02');
