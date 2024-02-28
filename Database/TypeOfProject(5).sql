133337d26ecdf5d1aa84d0ffe5b79c75d6be35db
hiennmse170279 <hiennmse170279@fpt.edu.vn>
2024-02-28 18:13:35 +0700

    Update

--- Database/TypeOfProject(5).sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `typeofprojects` (`id`, `projectID`, `typeID`, `createdAt`, `updatedAt`) VALUES
-(1, 1, 1, '2024-02-01 15:20:59', '2024-02-01 15:20:59');
-INSERT INTO `typeofprojects` (`id`, `projectID`, `typeID`, `createdAt`, `updatedAt`) VALUES
-(2, 1, 2, '2024-02-01 15:20:59', '2024-02-01 15:20:59');
-INSERT INTO `typeofprojects` (`id`, `projectID`, `typeID`, `createdAt`, `updatedAt`) VALUES
-(3, 2, 2, '2024-02-01 15:20:59', '2024-02-01 15:20:59');
-INSERT INTO `typeofprojects` (`id`, `projectID`, `typeID`, `createdAt`, `updatedAt`) VALUES
-(4, 3, 1, '2024-02-01 15:20:59', '2024-02-01 15:20:59'),
-(5, 4, 1, '2024-02-01 15:20:59', '2024-02-01 15:20:59');
