fb24c42cefbbf9788db2666b42d6ef7dba026594
Minh Hiển <hiennmse170279@fpt.edu.vn>
2024-02-29 23:50:13 +0700

    Update

--- Database/Users(6).sql
+++ Database/Users(6).sql
@@ -1,6 +1,9 @@
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
-(1, 'admin', 'admin@gmail.com', '123456', '123456789', 0, NULL, 1, NULL, NULL, '2024-02-25 22:49:51', '2024-02-25 22:49:51');
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
-(2, 'staff', 'staff@gmail.com', '123456', '123456789', 0, NULL, 2, NULL, NULL, '2024-02-25 22:50:21', '2024-02-25 22:50:21');
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
-(3, 'customer', 'customer@gmail.com', '123456', '123456789', 0, NULL, 3, NULL, NULL, '2024-02-25 22:51:02', '2024-02-25 22:51:02');
+INSERT INTO `users` (`id`, `username`, `fullName`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
+(1, 'admin', NULL, 'admin@gmail.com', '123456', '123456789', 0, NULL, 1, NULL, NULL, '2024-02-25 22:49:51', '2024-02-25 22:49:51');
+INSERT INTO `users` (`id`, `username`, `fullName`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
+(2, 'staff', NULL, 'staff@gmail.com', '123456', '123456789', 0, NULL, 2, NULL, NULL, '2024-02-25 22:50:21', '2024-02-25 22:50:21');
+INSERT INTO `users` (`id`, `username`, `fullName`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
+(3, 'customer', NULL, 'customer@gmail.com', '123456', '123456789', 0, NULL, 3, NULL, NULL, '2024-02-25 22:51:02', '2024-02-25 22:51:02');
+INSERT INTO `users` (`id`, `username`, `fullName`, `email`, `password`, `phoneNumber`, `banStatus`, `refundHistoryID`, `roleID`, `refreshToken`, `type`, `createdAt`, `updatedAt`) VALUES
+(4, 'staff1', NULL, 'staff1@gmail.com', '123456', '123456789', 0, NULL, 2, NULL, NULL, '2024-02-29 23:30:06', '2024-02-29 23:30:06'),
+(5, 'tivas', NULL, 'tivas@gmail.com', '123456', '123456789', 0, NULL, 3, NULL, NULL, '2024-02-29 23:30:06', '2024-02-29 23:30:06');
